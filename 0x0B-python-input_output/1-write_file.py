7e8bead14aba2a10c9febc9e8b8d11a481977909
Sadiq6604 <saddiqumukhtar67@gmail.com>
2023-04-12 22:21:44 +0000

    shma

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""The module defined  number_of_lines function"""
+
+
+"""Defines a text file line-counting function."""
 
 
 def number_of_lines(filename=""):
-    """Return number of line of a text file
-    Args:
-    filename (str): Filename
-    """
-    count = 0
-    with open(filename, encoding='utf-8') as file:
-        for line in file:
-            count += 1
-        return count
+    """Return the number of lines in a text file."""
+    lines = 0
+    with open(filename) as f:
+        for line in f:
+            lines += 1
+    return lines
+
