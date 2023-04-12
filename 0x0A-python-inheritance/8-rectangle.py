978addde9f74292913f6fee99b9ab3a2c323caa5
Sadiq6604 <saddiqumukhtar67@gmail.com>
2023-04-12 04:38:37 +0000

    sharp sharp

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+i#!/usr/bin/python3
 """ Import statement """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Class Rectangle s """
+    """ Rectangle Class  """
 
     def __init__(self, width, height):
         '''
