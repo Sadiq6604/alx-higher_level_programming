141ac12fbe9026cb0c0ef11ee30b6b84dff1c402
Sadiq6604 <saddiqumukhtar67@gmail.com>
2023-04-13 20:29:27 +0000

    e shock

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" The  Module that contains a func  that appends to a text file
+"""
+
+
+def append_write(filename="", text=""):
+    """ Func that appends to a text file
+    Args:
+        filename: filename
+        text: text to write
+    Raises
+        Exception: when the file can be opened
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
+Footer
+
